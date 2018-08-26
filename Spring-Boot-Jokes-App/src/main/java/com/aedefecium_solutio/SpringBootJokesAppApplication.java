9da141d09dde3a3a162afc8ec3ad5eada5694519
darm191065 <cheznl191065@gmail.com>
2018-08-25 22:28:07 -0500

    Commit 1

---
+++ Spring-Boot-Jokes-App/src/main/java/com/aedefecium_solutio/SpringBootJokesAppApplication.java
@@ -0,0 +1,12 @@
+package com.aedefecium_solutio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJokesAppApplication.class, args);
+	}
+}
